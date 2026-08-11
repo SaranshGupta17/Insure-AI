8ed41eb704d544bbb3a08c720774fc8165d6b6aa
Saransh Gupta <108832858+SaranshGupta17@users.noreply.github.com>
2026-08-11 23:52:02 +0530

    Add Analytics import to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import EmployeeDashboard from './components/officer/EmployeeDashboard';
 import CustomerDashboard from './components/customer/CustomerDashboard';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
